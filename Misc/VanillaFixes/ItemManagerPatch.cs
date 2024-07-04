5e070c649d0750748f4bd2f44e1e2b1e460aa70d
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2024-07-04 15:42:34 -0300

    ItemManagerPatch moved as a vanilla fix

---
+++ Misc/VanillaFixes/ItemManagerPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace PixelInternalAPI.Misc.VanillaFixes
+{
+    [HarmonyPatch(typeof(ItemManager))]
+    internal class ItemManagerPatch
+    {
+        [HarmonyPatch("AddItem", [typeof(ItemObject)])]
+        [HarmonyPatch("SetItem")]
+        [HarmonyPrefix]
+        static bool IsInstantUse(ItemObject item, PlayerManager ___pm)
+        {
+            if (!item.addToInventory)
+            {
+                Object.Instantiate(item.item).Use(___pm);
+                return false;
+            }
+            return true;
+        }
+    }
+}
